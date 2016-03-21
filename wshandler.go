7263b1f1d96db557c659559a0125cb30a1ef7b03
Lieuwe Rooijakkers <lieuwerooijakkers@gmail.com>
2016-03-21 18:34:37 +0100

    make the server ping

--- wshandler.go
+++ wshandler.go
@@ -4,8 +4,14 @@ import (
 	"bufio"
 	"log"
 	"strings"
+	"time"
 
 	"golang.org/x/net/websocket"
+)
+
+const (
+	pingTime    = 10 // in seconds
+	pingTimeout = 3  // amount of pings until timeout
 )
 
 func WsHandler(ws *websocket.Conn) {
@@ -27,28 +33,51 @@ func WsHandler(ws *websocket.Conn) {
 	}
 	defer room.RemoveWs(index)
 
-	for {
-		str, err = reader.ReadString('\n')
-		if err != nil {
-			log.Println(err)
-			return
-		}
+	pingsSinceLastMessage := 0
+	waitchan := make(chan int)
 
-		splitted := strings.Split(str, delim)
-		var otherIndex int
-		switch splitted[0] {
-		case "L":
-			otherIndex = index - 1
-		case "R":
-			otherIndex = index + 1
-		default:
-			ws.Write([]byte(MakeWsErr("invalid-side")))
-			continue
-		}
+	go func() {
+		for {
+			time.Sleep(time.Second * pingTime)
+			ws.Write([]byte("ping" + delim + "pong"))
 
-		if otherIndex >= 0 && otherIndex < len(room.sides) {
-			message := str[:len(str)-1]
-			room.sides[otherIndex].Write([]byte(message))
+			pingsSinceLastMessage++
+			if pingsSinceLastMessage == pingTimeout+1 {
+				log.Println("ws timed out, killing connection")
+				waitchan <- 1
+			}
 		}
-	}
+	}()
+
+	go func() {
+		for {
+			str, err = reader.ReadString('\n')
+			if err != nil {
+				waitchan <- 1
+			}
+
+			pingsSinceLastMessage = 0
+
+			splitted := strings.Split(str, delim)
+			var otherIndex int
+			switch splitted[0] {
+			case "L":
+				otherIndex = index - 1
+			case "R":
+				otherIndex = index + 1
+			case "pong":
+				break
+			default:
+				ws.Write([]byte(MakeWsErr("invalid-side")))
+				continue
+			}
+
+			if otherIndex >= 0 && otherIndex < len(room.sides) {
+				message := str[:len(str)-1]
+				room.sides[otherIndex].Write([]byte(message))
+			}
+		}
+	}()
+
+	<-waitchan
 }
